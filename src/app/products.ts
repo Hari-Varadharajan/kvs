ec4bb5e2d70389cea5ecdf93e3dc2c2eb927c371
Hari Varadharajan <harivaradharajan7@gmail.com>
2022-05-11 14:28:41 +0530

    schema-correctness1

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,9 @@
 export class Products {
     public p_id : string = "";
     public p_name : string = "";
+    public p_size : string = "";
+    public p_stock : Number = 0;
+    public p_price : Number = 0;
+    public p_sleev : string = "";
+    public p_neck : string = "";
 }
