03862478bb26dbe2225c87102d6e6b022d9fbadf
anojroshan <sulaianoj232001@gmail.com>
2022-05-16 23:17:29 +0530

    navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var VANTA:any
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    VANTA.BIRDS({
+      el: "#bgvanta2",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0xffffff,
+      color1: 0x4b91b,
+      color2: 0xcd2339,
+      quantity: 2.00
+    })
+    VANTA.WAVES({
+      el: "#bgvanta1",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x486b53
+    })
   }
+
+ 
 
 }
