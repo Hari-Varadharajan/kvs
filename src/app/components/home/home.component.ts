ad30edc3f2b2f852b8665b36bce5648f747ea456
anojroshan <sulaianoj232001@gmail.com>
2022-05-17 14:13:17 +0530

    bg

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,39 +14,34 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
    
-    // VANTA.BIRDS({
-    //   el: "#bgvanta2",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: false,
-    //   minHeight: 200.00,
-    //   minWidth: 200.00,
-    //   scale: 1.00,
-    //   scaleMobile: 1.00,
-    //   backgroundColor: 0xffffff,
-    //   color1: 0x4b91b,
-    //   color2: 0xcd2339,
-    //   quantity: 2.00
-    // })
-    // VANTA.WAVES({
-    //   el: "#bgvanta1",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: false,
-    //   minHeight: 200.00,
-    //   minWidth: 200.00,
-    //   scale: 1.00,
-    //   scaleMobile: 1.00,
-    //   color: 0x486b53
-    // })
+    VANTA.BIRDS({
+      el: "#bgvanta2",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0xffffff,
+      color1: 0x4b91b,
+      color2: 0xcd2339,
+      quantity: 2.00
+    })
+    VANTA.WAVES({
+      el: "#bgvanta1",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x486b53
+    })
   }
 
-  /*new TypeIt(".multipleStrings", {
-    strings: ["This is a great string.", "But here is a better one."],
-    speed: 50,
-    waitUntilVisible: true,
-  }).go();*/
-
+ 
  
 
 }
