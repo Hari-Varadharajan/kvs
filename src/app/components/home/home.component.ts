65963c8722c6a92b5d3a60ab432e6697a295d7f1
Hari Varadharajan <harivaradharajan7@gmail.com>
2022-05-18 12:25:50 +0530

    css corrected machi

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,6 +14,18 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
    
+
+        // VANTA.WAVES({
+    //   el: "#bgvanta1",
+    //   mouseControls: true,
+    //   touchControls: true,
+    //   gyroControls: false,
+    //   minHeight: 200.00,
+    //   minWidth: 200.00,
+    //   scale: 1.00,
+    //   scaleMobile: 1.00,
+    //   color: 0x486b53
+    // })
     VANTA.BIRDS({
       el: "#bgvanta2",
       mouseControls: true,
@@ -28,17 +40,7 @@ export class HomeComponent implements OnInit {
       color2: 0xcd2339,
       quantity: 2.00
     })
-    // VANTA.WAVES({
-    //   el: "#bgvanta1",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: false,
-    //   minHeight: 200.00,
-    //   minWidth: 200.00,
-    //   scale: 1.00,
-    //   scaleMobile: 1.00,
-    //   color: 0x486b53
-    // })
+
   }
 
  
